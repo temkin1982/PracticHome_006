4f339e0d7421b648c4bcbbf07828238b301ede52
temkin1982 <124079603+temkin1982@users.noreply.github.com>
2023-02-25 12:43:18 +0200

    HomeWork

---
+++ PracticHome007/Phone.cs
@@ -0,0 +1,55 @@
+﻿namespace PracticHome007
+{
+    
+    public class Phone
+    {
+        public string model = "ProMax";
+        public int number = 0509015021;
+        public int weight = 176;
+        public string modelM;
+        public int numberN;
+        public double weightG;
+        public string Call = "Artem";
+
+        //public void PhoneStats()
+        //{
+        //    Console.WriteLine($"The model name Phone: {model}\nNumber Phone: {number}  \nand weight Phone is: {weight} gram");
+        //}
+        //public void receiveCall()
+        //{
+        //    Console.WriteLine($"You have call from {Call}");
+        //}
+
+        //public Phone(string modelPhone, int numberPhone, double weightPhone)
+        //{
+
+        //    modelM = modelPhone;
+        //    numberN = numberPhone;
+        //    weightG = weightPhone;
+
+        //}
+        //public void ShowStats()
+        //{
+        //    Console.WriteLine($"The model name Phone: {modelM}\nNumber Phone: {numberN}");
+        //}
+        //public EnvironmentVariableTarget
+
+        //конструктор без параметров.
+        public Phone()
+        {
+
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+
+    }   
+}
---
+++ PracticHome007/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PracticHome007
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Phone proMax = new Phone("ProMax2", 0509015021, 174.5 );
+            //proMax.PhoneStats();
+            //proMax.receiveCall();
+            //Console.WriteLine("--------------------");
+            //proMax.ShowStats();
+
+            Phone obj = new Phone();
+        }
+    }
+}
