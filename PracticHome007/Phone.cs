1c98bccbcaef51da6e93500855cced40964b5390
temkin1982 <124079603+temkin1982@users.noreply.github.com>
2023-02-28 18:22:18 +0200

    update

--- PracticHome007/Phone.cs
+++ PracticHome007/Phone.cs
@@ -3,43 +3,73 @@
     
     public class Phone
     {
-        public string model = "ProMax";
-        public int number = 0509015021;
-        public int weight = 176;
-        public string modelM;
-        public int numberN;
-        public double weightG;
-        public string Call = "Artem";
+        public string model;
+        public int number;
+        public double weight;
+        
+        
 
-        //public void PhoneStats()
-        //{
-        //    Console.WriteLine($"The model name Phone: {model}\nNumber Phone: {number}  \nand weight Phone is: {weight} gram");
-        //}
-        //public void receiveCall()
-        //{
-        //    Console.WriteLine($"You have call from {Call}");
-        //}
+        public void PhoneStats()
+        {
+            Console.WriteLine($"The model name Phone: {model}\nNumber Phone: {number}  \nand weight Phone is: {weight} gram");
+        }
+        public void receiveCall(string nameCall)
+        {
+            Console.WriteLine($"You have call from {nameCall}");
+        }
 
-        //public Phone(string modelPhone, int numberPhone, double weightPhone)
-        //{
-
-        //    modelM = modelPhone;
-        //    numberN = numberPhone;
-        //    weightG = weightPhone;
-
-        //}
-        //public void ShowStats()
-        //{
-        //    Console.WriteLine($"The model name Phone: {modelM}\nNumber Phone: {numberN}");
-        //}
-        //public EnvironmentVariableTarget
-
-        //конструктор без параметров.
-        public Phone()
+        public Phone (double weightPhone) : this("MaxPro" ,0509015021)
         {
 
+            
+            weight = weightPhone;
+
         }
-    
+        public Phone (string modelPhone, int numberPhone)
+        {
+
+            this.model = modelPhone;
+            this.number = numberPhone;
+
+        }
+        public void ShowStats()
+        {
+            Console.WriteLine($"The model name Phone: {model}\nNumber Phone: {number}");
+        }
+        public void receiveCall(string nameCall, int number)
+        {
+            var result = nameCall.ToString() + number;
+           
+            Console.WriteLine($"You have call from {nameCall} number call {number}");
+            return;
+        }
+
+
+
+
+        //конструктор без параметров.
+        //public Phone()
+        //{
+
+        //}
+        public void Care(int number)
+        {
+            Console.WriteLine(number);
+
+        }
+
+        
+        public void Care(ref int speed)
+        {
+            speed = speed + 12;
+            Console.WriteLine(speed);
+        }
+       
+        public void Care(long mull  )
+        {
+            mull = mull * 2;
+            Console.WriteLine(mull);
+        }
     
     
     
--- PracticHome007/Program.cs
+++ PracticHome007/Program.cs
@@ -1,16 +1,32 @@
-﻿namespace PracticHome007
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PracticHome007
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            //Phone proMax = new Phone("ProMax2", 0509015021, 174.5 );
-            //proMax.PhoneStats();
-            //proMax.receiveCall();
-            //Console.WriteLine("--------------------");
-            //proMax.ShowStats();
+            Phone proMax = new Phone(174.6);
+            proMax.PhoneStats();
+            proMax.receiveCall("Artem");
+            Console.WriteLine("--------------------");
+            proMax.ShowStats();
+            proMax.receiveCall("Dina", 054696895);
+            
+            //Phone obj = new Phone();
+            //int number = 12;
+            //Phone car = new Phone();    
+            //car.Care(12);
+            //car.Care(ref number );
+            //car.Care((long)2);
 
-            Phone obj = new Phone();
         }
+
+         
+
+        
+        
+        
     }
 }
